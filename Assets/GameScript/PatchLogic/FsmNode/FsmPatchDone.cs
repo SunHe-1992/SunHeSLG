87836f81a515e6a828ffb6382ced930ff2e5a6a8
JohnSmith <claymore.sun@gmail.com>
2023-03-25 21:42:59 +0800

    Add YooAsset plugin

---
+++ Assets/GameScript/PatchLogic/FsmNode/FsmPatchDone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniFramework.Machine;
+using UniFramework.Module;
+
+/// <summary>
+/// 流程更新完毕
+/// </summary>
+internal class FsmPatchDone : IStateNode
+{
+	void IStateNode.OnCreate(StateMachine machine)
+	{
+	}
+	void IStateNode.OnEnter()
+	{
+		PatchEventDefine.PatchStatesChange.SendEventMessage("start game！");
+
+	}
+	void IStateNode.OnUpdate()
+	{
+	}
+	void IStateNode.OnExit()
+	{
+	}
+}
