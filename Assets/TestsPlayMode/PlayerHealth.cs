cb88d98f8027595e811b378ace6b08ca036976b2
Sun He <claymore.sun@gmail.com>
2024-07-13 22:17:04 -0400

    add test case : sort array

--- Assets/TestsPlayMode/PlayerHealth.cs
+++ Assets/TestsPlayMode/PlayerHealth.cs
@@ -23,4 +23,23 @@ public class PlayerHealth : MonoBehaviour
         if (currentHealth > maxHealth)
             currentHealth = maxHealth;
     }
+
+    public void SortBySpeed(ref int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
 }
--- Assets/TestsPlayMode/PlayerHealthTests.cs
+++ Assets/TestsPlayMode/PlayerHealthTests.cs
@@ -59,4 +59,19 @@ public class PlayerHealthTests
         playerHealth.Heal(healAmount);
         Assert.AreEqual(playerHealth.maxHealth, playerHealth.currentHealth);
     }
+
+    [Test]
+    public void PlayerHealth_SortBySpeed()
+    {
+        int[] speedArray = new int[20] { 45, 50, 55, 60, 65, 70, 75, 80, 85, 90,
+         95, 100, 105, 110, 115, 120, 125, 130, 135, 140 };
+        playerHealth.SortBySpeed(ref speedArray);
+        Debug.Break();
+        string print = "";
+        foreach (int value in speedArray)
+        {
+            print += (value + " ");
+        }
+        Debug.Log(print);
+    }
 }
